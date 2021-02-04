d9ca0876af765a482d640e6e28e338a2a9b08f4c
Pawan Kartik <SatyaPawanK@outlook.com>
2021-02-05 01:45:07 +0530

    Code cleanup

--- src/main/scala/sjvm/Main.scala
+++ src/main/scala/sjvm/Main.scala
@@ -1,15 +1,7 @@
 package sjvm
 
-import sjvm.classfile.JClass
-import sjvm.runtime.VirtualMachine
-
-import scala.collection.mutable
-
 object Main {
   def main(args: Array[String]): Unit = {
-    val classes = mutable.HashMap[String, JClass]()
-    val cmdlineArguments = new CmdlineArguments(args)
-    val virtualMachine = new VirtualMachine(cmdlineArguments)
-    virtualMachine.run()
+    new sjvm.runtime.VirtualMachine(new CmdlineArguments(args)).run()
   }
 }
